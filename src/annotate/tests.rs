cb520b1bab74271fb9e93f9a2552b848e5199cd1
Nate Wilcox <nathan+dev@electriccoin.co>
2022-01-26 06:04:38 -0800

    Add another test pair for Paths.

--- src/annotate/tests.rs
+++ src/annotate/tests.rs
@@ -23,3 +23,39 @@ mod string {
         assert_eq!(source, r#"Input "apple" != "banana""#);
     }
 }
+
+mod path {
+    use crate::Annotate;
+    use std::path::{Path, PathBuf};
+
+    fn is_root(input: &Path) -> Result<(), String> {
+        if let Some(parent) = input.parent() {
+            Err(format!(
+                "Input {:?} has parent {:?}",
+                input.display(),
+                parent.display()
+            ))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn within_ok() {
+        let pb = PathBuf::from("/");
+        let r = Annotate::within(pb.as_path(), is_root);
+        assert!(r.is_ok());
+    }
+
+    #[test]
+    fn within_err() {
+        let pb = PathBuf::from("/not/a/root/path");
+        let r = Annotate::within(pb.as_path(), is_root);
+        let Annotate { info, source } = r.err().unwrap();
+        assert_eq!(info, pb);
+        assert_eq!(
+            source,
+            r#"Input "/not/a/root/path" has parent "/not/a/root""#
+        );
+    }
+}

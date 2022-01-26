af874f963b35e7cb30b496ef09c0de52b5a07c7b
Nate Wilcox <nathan+dev@electriccoin.co>
2022-01-26 05:58:25 -0800

    Add a test case for error annotation.

--- src/annotate/tests.rs
+++ src/annotate/tests.rs
@@ -1,15 +1,25 @@
-use crate::Annotate;
+mod string {
+    use crate::Annotate;
 
-fn is_banana(input: &str) -> Result<(), String> {
-    if input == "banana" {
-        Ok(())
-    } else {
-        Err(format!("Input {:?} != \"banana\"", input))
+    fn is_banana(input: &str) -> Result<(), String> {
+        if input == "banana" {
+            Ok(())
+        } else {
+            Err(format!(r#"Input {:?} != "banana""#, input))
+        }
     }
-}
 
-#[test]
-fn within_no_error() {
-    let r = Annotate::within("banana", is_banana);
-    assert!(r.is_ok());
+    #[test]
+    fn within_ok() {
+        let r = Annotate::within("banana", is_banana);
+        assert!(r.is_ok());
+    }
+
+    #[test]
+    fn within_err() {
+        let r = Annotate::within("apple", is_banana);
+        let Annotate { info, source } = r.err().unwrap();
+        assert_eq!(info, "apple");
+        assert_eq!(source, r#"Input "apple" != "banana""#);
+    }
 }

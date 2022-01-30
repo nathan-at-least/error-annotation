357d3984202c3a8c885d4a22a7e55f6a913e663f
Nate Wilcox <nathan+dev@electriccoin.co>
2022-01-29 21:54:25 -0800

    Get the motivating example on the crate-level as the first example.

--- src/annotatable.rs
+++ src/annotatable.rs
@@ -1,47 +1,14 @@
 use crate::ErrorAnnotation;
 use std::fmt::Display;
 
-/// An `Annotatable` error type is can convert an `ErrorAnnotation` with `Self` as the source type
-/// back into `Self`, provided the annotated `info` implements `Display`.
+/// An `Annotatable` error type is can convert an [ErrorAnnotation] with `Self` as the source type
+/// back into `Self`, provided the annotated `info` implements [std::fmt::Display].
 ///
-/// A prime example is the `std::io::Error` impl, which can be used to convert any annotation on a
-/// `std::io::Error` back into a `std::io::Error`.
-///
-/// Errors which implement `Annotatable` allow code to introduce annotations without all of the
-/// various diagnostic information resulting in a plethora of uniquely parameterized
-/// `ErrorAnnotation` types. This can be convenient, for example, to prevent `ErrorAnnotation`
-/// types from leaking out into a crate's interface, so that consumers need only handle common base
-/// error types, such as `std::io::Error`.
+/// A prime example is the [std::io::Error] impl, which can be used to convert any annotation on a
+/// [std::io::Error] back into a [std::io::Error], as the [`crate`]-level example demonstrates.
 ///
 /// The conversion provided by implementations of `Annotatable` is typically conveniently
-/// accomplished via `AnnotateResult::annotate_err_into`.
-///
-/// # Example
-///
-/// ```
-/// use std::path::{Path, PathBuf};
-/// use std::fs::Metadata;
-/// use error_annotation::{AnnotateResult, ErrorAnnotation};
-///
-/// fn annotated_copy(src: &Path, dst: &Path) -> std::io::Result<u64> {
-///   std::fs::copy(src, dst)
-///     .annotate_err_into("source", || src.display())
-///     .annotate_err_into("destination", || dst.display())
-/// }
-///
-/// let badsource = PathBuf::from("/this/path/does/not/exist");
-/// let dest = PathBuf::from("/tmp/woah-dude");
-/// let res = annotated_copy(&badsource, &dest);
-/// let err = res.err().unwrap();
-///
-/// assert_eq!(&err.to_string(), "
-///
-/// No such file or directory (os error 2)
-/// -with source: /this/path/does/not/exist
-/// -with destination: /tmp/woah-dude
-///
-/// ".trim());
-/// ```
+/// accomplished via [AnnotateResult::annotate_err_into](crate::AnnotateResult::annotate_err_into).
 pub trait Annotatable: Sized {
     fn merge_annotation<I>(ea: ErrorAnnotation<Self, I>) -> Self
     where
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,46 @@
 //! Add useful diagnostic information to error values as they propagate.
 //!
-//! The recommended way to annotate errors directly from a `Result` value is with the
-//! [`AnnotateResult::annotate_err`] method which is implemented on `Result`.
+//! # Annotatable Error Types
+//!
+//! The most ergonomic usage is available for types that implement [`Annotatable`] via the
+//! [`AnnotateResult::annotate_err_into`] method, which is implemented for `Result`. In this case,
+//! diagnostic annotations can be built up without altering the resulting error types. An example
+//! using the `std::io::Error` impl of `Annotatable` clarifies these ergonomics:
+//!
+//! ## Example: Chaining Diagnostic Annotations on `std::io::Error`
+//!
+//! ```
+//! use std::path::{Path, PathBuf};
+//! use std::fs::Metadata;
+//! use error_annotation::{AnnotateResult, ErrorAnnotation};
+//!
+//! fn annotated_copy(src: &Path, dst: &Path) -> std::io::Result<u64> {
+//!   std::fs::copy(src, dst)
+//!     .annotate_err_into("source", || src.display())
+//!     .annotate_err_into("destination", || dst.display())
+//! }
+//!
+//! let badsource = PathBuf::from("/this/path/does/not/exist");
+//! let dest = PathBuf::from("/tmp/woah-dude");
+//! let res = annotated_copy(&badsource, &dest);
+//! let err = res.err().unwrap();
+//!
+//! assert_eq!(&err.to_string(), "
+//!
+//! No such file or directory (os error 2)
+//! -with source: /this/path/does/not/exist
+//! -with destination: /tmp/woah-dude
+//!
+//! ".trim());
+//! ```
+//!
+//! # Annotating Other Error Types
+//!
+//! It is still possible to annotate an arbitrary error type `T` which does not implement [`Annotatable`]
+//! with annotation info type `I` by way of the [`ErrorAnnotation`]
+//! parameterized type. The downside being each annotation corresponds to a different
+//! parameterization, ie `ErrorAnnotation<T, I>`, which propagates out of interfaces and must be
+//! explicitly handled by consuming code.
 
 mod annotatable;
 mod annotate;

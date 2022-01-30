1a1043a27f3a845d8b2a359f4b865f00c33ba268
Nate Wilcox <nathan+dev@electriccoin.co>
2022-01-29 20:04:27 -0800

    Add doc strings; remove `ErrorAnnotation::annotate` in favor of just `annotate`.

--- src/annres.rs
+++ src/annres.rs
@@ -1,5 +1,20 @@
 use crate::{annotate, ErrorAnnotation};
 
+/// A trait to extend `Result` with a convenient `annotate_err` method. This is the recommended
+/// interface for annotating errors directly on `Result` values.
+///
+/// # Example
+///
+/// ```
+/// use std::path::Path;
+/// use error_annotation::{AnnotateResult, ErrorAnnotation};
+///
+/// type IoErrorWithPath<'a> = ErrorAnnotation<std::io::Error, std::path::Display<'a>>;
+///
+/// fn remove_dir_all(p: &Path) -> Result<(), IoErrorWithPath> {
+///   std::fs::remove_dir_all(p).annotate_err("path", || p.display())
+/// }
+/// ```
 pub trait AnnotateResult<T, E> {
     fn annotate_err<F, I>(self, label: &'static str, mkinfo: F) -> Result<T, ErrorAnnotation<E, I>>
     where
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! Add useful diagnostic information to error values as they propagate.
+//!
+//! The recommended way to annotate errors directly from a `Result` value is with the
+//! [`AnnotateResult::annotate_err`] method which is implemented on `Result`.
+
 mod annres;
 mod ea;
 
